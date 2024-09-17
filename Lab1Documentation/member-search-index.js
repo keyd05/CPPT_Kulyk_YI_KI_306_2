f26ca6d893484cd591396e10acaad396e35e0ebe
keyd05 <yurakulyk321@gmail.com>
2024-09-18 00:30:34 +0300

    Lab1

---
+++ Lab1Documentation/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Lab1Kulyk306","l":"Lab1Kulyk306()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Lab1Kulyk306","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
